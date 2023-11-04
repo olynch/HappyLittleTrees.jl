a6f2d8d17ad468bdd9a3bb8535a829aba36eddc9
Owen Lynch <root@owenlynch.org>
2023-11-03 22:43:06 -0400

    longer docstring

--- src/HappyLittleTrees.jl
+++ src/HappyLittleTrees.jl
@@ -13,8 +13,32 @@ ArrayMappedTrie
 An efficient mapping from UInt64 to values. Performs about ~2-3 times slower
   than a Julia Dict and uses 1-2 times as much memory. However, can be used in
   a persistent/copy-on-write manner without much degradation of performance.
+  Additionally, stores the data in sorted order with better performance than
+  a binary tree.
 
-TODO: make persistent version
+Reference: http://lampwww.epfl.ch/papers/idealhashtrees.pdf
+
+Conceptually, you can think of this as
+
+struct AMT{T}
+  children::SVector{64, Union{Nothing, Pair{UInt64, T}, AMT{T}}}
+end
+
+This is then indexed in the normal trie manner, i.e. you treat a UInt64
+  as an array of 6 bit chunks, and follow a path down the tree until you hit
+  a leaf.
+
+However, this would be horribly space inefficient. Therefore, we instead
+  store using a UInt64 as a bitmap the array indices that are leaves,
+  and we use another UInt64 to store the array indices that are other
+  nodes. We can then store only the non-null leaves/children in separate
+  arrays. To index into these arrays, we take advantage of the CPU
+  "popcount" instruction, which counts the number of 1s in a word.
+  By masking out the higher bits, we can in two CPU instructions count
+  the number of bits up to a given bit, which gives us our indices
+  into the keyvals/children arrays.
+
+TODO: make persistent version.
 """
 mutable struct AMT{T}
   keyvals::Vector{Pair{UInt64, T}}
